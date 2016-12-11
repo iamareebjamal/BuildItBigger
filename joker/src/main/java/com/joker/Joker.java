cdd628311e482ad246a25db3a93ebb430f895352
iamareebjamal <jamal.areeb@gmail.com>
2016-12-11 18:38:53 +0530

    Added new jokes

--- joker/src/main/java/com/joker/Joker.java
+++ joker/src/main/java/com/joker/Joker.java
@@ -8,13 +8,6 @@ public class Joker {
 
     static {
         jokes = Joke.addJokes(
-                "Two bytes meet.  The first byte asks, “Are you ill?”  \n" +
-                "The second byte replies, “No, just feeling a bit off.”",
-
-                "Eight bytes walk into a bar.  The bartender asks, “Can I get you anything?”\n" +
-                "\n" +
-                "“Yeah,” reply the bytes.  “Make us a double.”",
-
                 "Why do programmers always mix up Halloween and Christmas? \n" +
                 "Because Oct 31 equals Dec 25.",
 
@@ -26,14 +19,290 @@ public class Joker {
                 "very long pause….\n" +
                 "“Java.”",
 
-                "Programming is like sex:\n" +
+                "(NSFW)\n\nProgramming is like sex:\n" +
                 "One mistake and you have to support it for the rest of your life.",
 
                 "A man is smoking a cigarette and blowing smoke rings into the air.  " +
                 "His girlfriend becomes irritated with the smoke and says, " +
                 "“Can’t you see the warning on the cigarette pack?  Smoking is hazardous to your health!” \n" +
                 "\n" +
-                "To which the man replies, “I am a programmer.  We don’t worry about warnings; we only worry about errors.”"
+                "To which the man replies, “I am a programmer.  We don’t worry about warnings; we only worry about errors.”",
+
+                "A man flying in a hot air balloon suddenly realizes he’s lost. He reduces height and spots a man down below. He lowers the balloon further and shouts to get directions, \"Excuse me, can you tell me where I am?\"\n" +
+                        "\n" +
+                        "The man below says: \"Yes. You're in a hot air balloon, hovering 30 feet above this field.\"\n" +
+                        "\n" +
+                        "\"You must work in Information Technology,\" says the balloonist.\n" +
+                        "\n" +
+                        "\"I do\" replies the man. \"How did you know?\"\n" +
+                        "\n" +
+                        "\"Well,\" says the balloonist, \"everything you have told me is technically correct, but It's of no use to anyone.\"\n" +
+                        "\n" +
+                        "The man below replies, \"You must work in management.\"\n" +
+                        "\n" +
+                        "\"I do,\" replies the balloonist, *\"But how'd you know?\"**\n" +
+                        "\n" +
+                        "\"Well\", says the man, \"you don’t know where you are or where you’re going, but you expect me to be able to help. You’re in the same position you were before we met, but now it’s my fault.\"",
+
+
+                "A SQL query goes into a bar, walks up to two tables and asks, \"Can I join you?\"",
+
+                "A physicist, an engineer and a programmer were in a car driving over a steep alpine pass when the brakes failed. The car was getting faster and faster, they were struggling to get round the corners and once or twice only the feeble crash barrier saved them from crashing down the side of the mountain. They were sure they were all going to die, when suddenly they spotted an escape lane. They pulled into the escape lane, and came safely to a halt.\n" +
+                        "\n" +
+                        "The physicist said \"We need to model the friction in the brake pads and the resultant temperature rise, see if we can work out why they failed\".\n" +
+                        "\n" +
+                        "The engineer said \"I think I've got a few spanners in the back. I'll take a look and see if I can work out what's wrong\".\n" +
+                        "\n" +
+                        "The programmer said \"Why don't we get going again and see if it's reproducible?\"",
+
+                "When your hammer is C++, everything begins to look like a thumb.",
+
+                "A computer science student is studying under a tree and another pulls up on a flashy new bike. The first student asks, “Where’d you get that?”\n" +
+                        "\n" +
+                        "The student on the bike replies, “While I was studying outside, a beautiful girl pulled up on her bike. She took off all her clothes and said, ‘You can have anything you want’.”\n" +
+                        "\n" +
+                        "The first student responds, “Good choice! Her clothes probably wouldn’t have fit you.”",
+
+                "If you put a million monkeys at a million keyboards, one of them will eventually write a Java program.\n" +
+                        "\n" +
+                        "The rest of them will write Perl programs.",
+
+                "Q: \"Whats the object-oriented way to become wealthy?\"\n" +
+                        "\n" +
+                        "A: Inheritance",
+
+                "long long ago;" +
+                        "float titanic;" +
+                        "char mander;" +
+                        "int ezaar;",
+
+                "[\"hip\",\"hip\"]\n" +
+                        "\n" +
+                        "(hip hip array!)",
+
+                "A Cobol programmer made so much money doing Y2K remediation that he was able to have himself cryogenically frozen when he died. One day in the future, he was unexpectedly resurrected.\n" +
+                        "\n" +
+                        "When he asked why he was unfrozen, he was told:\n" +
+                        "\n" +
+                        "\"It's the year 9999 - and you know Cobol\"",
+
+                "(NSFW)\n\nSoftware is like sex: It's better when it's free. (Linus Torvalds)",
+
+                "Q: How many prolog programmers does it take to change a lightbulb?\n" +
+                        "\n" +
+                        "A: Yes.",
+
+                "To understand what recursion is, you must first understand recursion.",
+
+                "So this programmer goes out on a date with a hot chick.\n\n" +
+                        "Get it? It\'s a joke!\nNo?\n\nI'll show myself out",
+
+                "Jesus and Satan have an argument as to who is the better programmer. This goes on for a few hours until they come to an agreement to hold a contest with God as the judge. They set themselves before their computers and begin. They type furiously, lines of code streaming up the screen, for several hours straight.\n" +
+                        "\n" +
+                        "Seconds before the end of the competition, a bolt of lightning strikes, taking out the electricity. Moments later, the power is restored, and God announces that the contest is over. He asks Satan to show his work. Visibly upset, Satan cries and says, “I have nothing. I lost it all when the power went out.”\n" +
+                        "\n" +
+                        "“Very well,” says God, “let us see if Jesus has fared any better.”\n" +
+                        "\n" +
+                        "Jesus presses a key, and the screen comes to life in vivid display, the voices of an angelic choir pour forth from the speakers.\n" +
+                        "\n" +
+                        "Satan is astonished. He stutters, “B-b-but how?! I lost everything, yet Jesus’ program is intact! How did he do it?”\n" +
+                        "\n" +
+                        "God chuckles, “Everybody knows… Jesus saves.”",
+
+                "char*lie;\n" +
+                        "\n" +
+                        "    double time, me= !0XFACE,\n" +
+                        "\n" +
+                        "    not; int rested,   get, out;\n" +
+                        "\n" +
+                        "    main(ly, die) char ly, **die ;{\n" +
+                        "\n" +
+                        "        signed char lotte,\n" +
+                        "\n" +
+                        "\n" +
+                        "dear; (char)lotte--;\n" +
+                        "\n" +
+                        "    for(get= !me;; not){\n" +
+                        "\n" +
+                        "    1 -  out & out ;lie;{\n" +
+                        "\n" +
+                        "    char lotte, my= dear,\n" +
+                        "\n" +
+                        "    **let= !!me *!not+ ++die;\n" +
+                        "\n" +
+                        "        (char*)(lie=\n" +
+                        "\n" +
+                        "\n" +
+                        "\"The gloves are OFF this time, I detest you, snot\\n\\0sed GEEK!\");\n" +
+                        "\n" +
+                        "    do {not= *lie++ & 0xF00L* !me;\n" +
+                        "\n" +
+                        "    #define love (char*)lie -\n" +
+                        "\n" +
+                        "    love 1s *!(not= atoi(let\n" +
+                        "\n" +
+                        "    [get -me?\n" +
+                        "\n" +
+                        "        (char)lotte-\n" +
+                        "\n" +
+                        "\n" +
+                        "(char)lotte: my- *love -\n" +
+                        "\n" +
+                        "    'I'  -  *love -  'U' -\n" +
+                        "\n" +
+                        "    'I'  -  (long)  - 4 - 'U' ])- !!\n" +
+                        "\n" +
+                        "    (time  =out=  'a'));} while( my - dear\n" +
+                        "\n" +
+                        "    && 'I'-1l  -get-  'a'); break;}}\n" +
+                        "\n" +
+                        "        (char)*lie++;\n" +
+                        "\n" +
+                        "\n" +
+                        "(char)*lie++, (char)*lie++; hell:0, (char)*lie;\n" +
+                        "\n" +
+                        "    get *out* (short)ly   -0-'R'-  get- 'a'^rested;\n" +
+                        "\n" +
+                        "    do {auto*eroticism,\n" +
+                        "\n" +
+                        "    that; puts(*( out\n" +
+                        "\n" +
+                        "        - 'c'\n" +
+                        "\n" +
+                        "-('P'-'S') +die+ -2 ));}while(!\"you're at it\");\n" +
+                        "\n" +
+                        "\n" +
+                        "for (*((char*)&lotte)^=\n" +
+                        "\n" +
+                        "    (char)lotte; (love ly) [(char)++lotte+\n" +
+                        "\n" +
+                        "    !!0xBABE];){ if ('I' -lie[ 2 +(char)lotte]){ 'I'-1l ***die; }\n" +
+                        "\n" +
+                        "    else{ if ('I' * get *out* ('I'-1l **die[ 2 ])) *((char*)&lotte) -=\n" +
+                        "\n" +
+                        "    '4' - ('I'-1l); not; for(get=!\n" +
+                        "\n" +
+                        "\n" +
+                        "get; !out; (char)*lie  &  0xD0- !not) return!!\n" +
+                        "\n" +
+                        "    (char)lotte;}\n" +
+                        "\n" +
+                        "\n" +
+                        "(char)lotte;\n" +
+                        "\n" +
+                        "    do{ not* putchar(lie [out\n" +
+                        "\n" +
+                        "    *!not* !!me +(char)lotte]);\n" +
+                        "\n" +
+                        "    not; for(;!'a';);}while(\n" +
+                        "\n" +
+                        "        love (char*)lie);{\n" +
+                        "\n" +
+                        "\n" +
+                        "register this; switch( (char)lie\n" +
+                        "\n" +
+                        "    [(char)lotte] -1s *!out) {\n" +
+                        "\n" +
+                        "    char*les, get= 0xFF, my; case' ':\n" +
+                        "\n" +
+                        "    *((char*)&lotte) += 15; !not +(char)*lie*'s';\n" +
+                        "\n" +
+                        "    this +1s+ not; default: 0xF +(char*)lie;}}}\n" +
+                        "\n" +
+                        "    get - !out;\n" +
+                        "\n" +
+                        "    if (not--)\n" +
+                        "\n" +
+                        "    goto hell;\n" +
+                        "\n" +
+                        "        exit( (char)lotte);}",
+
+                "(NSFW)\n\nThere are 10 types of people in the world. Those who understand binary and those who get laid.",
+
+                "Here's one I came up with many, many, many years ago:\n" +
+                        "\n" +
+                        "I called the janitor the other day to see what he could do about my dingy linoleum floor. " +
+                        "He said he would have been happy to loan me a polisher, but that he hadn't the slightest idea what he " +
+                        "had done with it. I told him not to worry about it - that as a programmer it wasn't the first" +
+                        " time I had experienced a buffer allocation failure due to a memory error.",
+
+                "(NSFW)\n\nWhy programmers like UNIX:\n" +
+                        "\n" +
+                        "unzip, strip, touch, finger, grep, mount, fsck, more, yes, fsck, fsck, fsck, umount, sleep",
+
+                "These two strings walk into a bar and sit down. The bartender says, \"So what'll it be?\"\n" +
+                        "\n" +
+                        "The first string says, \"I think I'll have a beer quag fulk boorg jdk^CjfdLk jk3s d#f67howe%^U r89nvy owmc63^Dz x.xvcu\"\n" +
+                        "\n" +
+                        "\"Please excuse my friend,\" the second string says, \"He isn't null-terminated.\"",
+
+                "There are 2 hard problems in computer science: caching, naming, and off-by-1 errors",
+
+                "A young man was walking along in the forest, when he heard a muffled voice crying for help from behind a log.  He leaned over to see a frog sitting in the mud.\n" +
+                        "\n" +
+                        "The frog looked up at him and said, \"I'm actually a beautiful princess, and if you kiss me, I'll transform back into my true self, and be yours for eternity.\"\n" +
+                        "\n" +
+                        "Silently, the man scooped up the frog and continued on his walk.\n" +
+                        "\n" +
+                        "A minute or two later, the frog piped up again, \"Hey, buddy, maybe you didn't hear me -- I said, if you kiss me, I'll turn into a princess. What are you waiting for?\"\n" +
+                        "\n" +
+                        "Annoyed, the man stuffed the frog into his coat pocket.\n" +
+                        "\n" +
+                        "Shocked, the frog yelled from inside the man's pocket, \"What the hell?  I'm a princess!  All you have to do is kiss me!\"\n" +
+                        "\n" +
+                        "Opening his pocket and peering in, the man said, \"Listen -- I'm a software engineer.  I don't have time for a girlfriend.  But a talking frog is kind of cool.\"",
+
+                "Why did the chicken cross the road?\n" +
+                        "\n" +
+                        "Assembler Chicken: First, it builds the road ......\n" +
+                        "\n" +
+                        "C Chicken: It crosses the road without looking both ways.\n" +
+                        "\n" +
+                        "C++ Chicken: The chicken wouldn't have to cross the road, you' d simply refer to him on the other side.\n" +
+                        "\n" +
+                        "COBOL Chicken: 0001-CHICKEN-CROSSING.\n" +
+                        "   IF NO-MORE-VEHICLES\n" +
+                        "        THEN PERFORM 0010-CROSS-THE-ROAD\n" +
+                        "        VARYING STEPS FROM 1 BY 1 UNTIL\n" +
+                        "        ON-THE-OTHER-SIDE\n" +
+                        "    ELSE\n" +
+                        "    GO TO 0001-CHICKEN-CROSSING\n" +
+                        "\n" +
+                        "Cray Chicken: Crosses faster than any other chicken, but if you don't dip it in liquid nitrogen first, it arrives on the other side frazzled.\n" +
+                        "\n" +
+                        "Delphi Chicken: The chicken is dragged across the road and dropped on the other side.\n" +
+                        "\n" +
+                        "Gopher Chicken: Tried to run but got beaten by the Web chicken.\n" +
+                        "\n" +
+                        "Intel Pentium Chicken: The chicken crossed 4.9999978 times.\n" +
+                        "\n" +
+                        "Iomega Chicken: The chicken should have ' backed up' before crossing.\n" +
+                        "\n" +
+                        "Java Chicken: If your road needs to be crossed by a chicken, then the server will download one to the other side. (Of course, those are chicklets.)  See also WMI Monitor.\n" +
+                        "\n" +
+                        "Linux Chicken: Don't you *dare* try to cross the road the same way we do!\n" +
+                        "\n" +
+                        "Mac Chicken: No reasonable chicken owner would want a chicken to cross the road, so there's no way to tell it how to cross the road.\n" +
+                        "\n" +
+                        "Newton Chicken: Can't cluck, can't fly, and can't lay eggs, but you can carry it across the road in your pocket.\n" +
+                        "\n" +
+                        "OOP Chicken: It doesn't need to cross the road, it just sends a message.\n" +
+                        "\n" +
+                        "OS/2 Chicken: It crossed the road in style years ago, but it was so quiet that nobody noticed.\n" +
+                        "\n" +
+                        "Microsoft's Chicken: It's already on both sides of the road. What's more its just bought the road.\n" +
+                        "\n" +
+                        "Windows 95 Chicken: You see different coloured feathers while it crosses, but when you cook it still tastes like........ chicken.\n" +
+                        "\n" +
+                        "Quantum Logic Chicken: The chicken is distributed probabilistically on all sides of the road until you observe it on the side of your choice.\n" +
+                        "\n" +
+                        "VB Chicken: USHighways! <TheRoad.cross> (aChicken)\n" +
+                        "\n" +
+                        "XP Chicken Jumps out onto the road, turns right, and just keeps on running.\n" +
+                        "\n" +
+                        "The Longhorn Chicken had an identity crisis and is now calling itself Vista.\n" +
+                        "\n" +
+                        "The Vista Chicken dazzled itself with its own graphics."
         );
     }
 
